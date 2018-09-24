b5c99c5b4b16b3874bf07ef30465182dc5c77a12
ziovik <mondaydaniel2002@yahoo.com>
2018-09-24 12:23:10 +0300

    change

--- optimum_beauty.php
+++ optimum_beauty.php
@@ -323,6 +323,78 @@
                                     </div>
                                 </div>
                                 <!-- /Product Single -->
+                                <!-- Product Single -->
+                                <div class="product product-single">
+                                    <div class="product-thumb">
+                                        <div class="product-label">
+                                            <span>New</span>
+                                            <span class="sale">-20%</span>
+                                        </div>
+                                        <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view
+                                        </button>
+                                        <img src="img/bm.jpg" alt="">
+                                    </div>
+                                    <div class="product-body">
+                                        <h3 class="product-price">Бьюти Маркет
+                                            <del class="product-old-price"></del>
+                                        </h3>
+                                        <div class="product-rating">
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star-o empty"></i>
+                                        </div>
+                                        <h2 class="product-name"><a href="#">Дистрибьютор</a></h2>
+                                        <div class="product-btns">
+                                            <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
+                                            <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
+                                            <input type="submit" class="primary-btn add-to-cart" style="width: 120px; float: right; color: white;" value="Продукты" onclick="profcosmaterial()">
+                                            <script type="text/javascript">
+                                                function profcosmaterial() {
+                                                    window.location = "customer/profcosmaterial.php";
+                                                }
+                                            </script>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- /Product Single -->
+                                <!-- Product Single -->
+                                <div class="product product-single">
+                                    <div class="product-thumb">
+                                        <div class="product-label">
+                                            <span>New</span>
+                                            <span class="sale">-20%</span>
+                                        </div>
+                                        <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view
+                                        </button>
+                                        <img src="img/bes.jpg" alt="">
+                                    </div>
+                                    <div class="product-body">
+                                        <h3 class="product-price">BES <small>RUSSIA</small>
+                                            <del class="product-old-price"></del>
+                                        </h3>
+                                        <div class="product-rating">
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star-o empty"></i>
+                                        </div>
+                                        <h2 class="product-name"><a href="#">Дистрибьютор</a></h2>
+                                        <div class="product-btns">
+                                            <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
+                                            <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
+                                            <input type="submit" class="primary-btn add-to-cart" style="width: 120px; float: right; color: white;" value="Продукты" onclick="profcosmaterial()">
+                                            <script type="text/javascript">
+                                                function profcosmaterial() {
+                                                    window.location = "customer/profcosmaterial.php";
+                                                }
+                                            </script>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- /Product Single -->
                             </div>
                         </div>
                     </div>
